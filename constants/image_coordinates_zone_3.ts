dfb3d8377acf303cc0d97394fb133bded635dd80
Hugo Saldarriaga <hugosmm2000@gmail.com>
2025-02-18 08:19:08 -0500

    feat: Add new constants points for each parking

---
+++ constants/image_coordinates_zone_3.ts
@@ -0,0 +1,54 @@
+import {Zones} from "@/types";
+
+
+const zone3 : Zones = [
+	// A la izquierda de la imagen.
+	{'id_parqueadero':'185', 'x':197, 'y':323},
+	{'id_parqueadero':'186', 'x':197, 'y':375},
+	{'id_parqueadero':'187', 'x':197, 'y':427},
+	{'id_parqueadero':'188', 'x':197, 'y':479},
+	{'id_parqueadero':'189', 'x':197, 'y':533},
+	{'id_parqueadero':'190', 'x':197, 'y':587},
+	{'id_parqueadero':'191', 'x':197, 'y':641},
+	{'id_parqueadero':'192', 'x':197, 'y':695},
+	{'id_parqueadero':'193', 'x':197, 'y':747},
+	{'id_parqueadero':'194', 'x':197, 'y':801},
+	{'id_parqueadero':'195', 'x':197, 'y':853},
+	{'id_parqueadero':'196', 'x':197, 'y':907},
+	// A centro de la imagen, en la izquierda.
+	{'id_parqueadero':'197', 'x':419, 'y':323},
+	{'id_parqueadero':'198', 'x':419, 'y':370},
+	{'id_parqueadero':'199', 'x':419, 'y':420},
+	{'id_parqueadero':'200', 'x':419, 'y':472},
+	{'id_parqueadero':'201', 'x':419, 'y':525},
+	{'id_parqueadero':'202', 'x':419, 'y':575},
+	{'id_parqueadero':'203', 'x':419, 'y':621},
+	{'id_parqueadero':'204', 'x':419, 'y':675},
+	{'id_parqueadero':'205', 'x':419, 'y':727},
+	{'id_parqueadero':'206', 'x':419, 'y':780},
+	{'id_parqueadero':'207', 'x':419, 'y':833},
+	// A centro de la imagen, en la derecha.
+	{'id_parqueadero':'208', 'x':533, 'y':323},
+	{'id_parqueadero':'209', 'x':533, 'y':370},
+	{'id_parqueadero':'210', 'x':533, 'y':420},
+	{'id_parqueadero':'211', 'x':533, 'y':472},
+	{'id_parqueadero':'212', 'x':533, 'y':525},
+	{'id_parqueadero':'213', 'x':533, 'y':575},
+	{'id_parqueadero':'214', 'x':533, 'y':621},
+	{'id_parqueadero':'215', 'x':533, 'y':675},
+	{'id_parqueadero':'216', 'x':533, 'y':727},
+	{'id_parqueadero':'217', 'x':533, 'y':780},
+	{'id_parqueadero':'218', 'x':533, 'y':833},
+	// A la derecha de la imagen.
+	{'id_parqueadero':'219', 'x':759, 'y':493},
+	{'id_parqueadero':'220', 'x':759, 'y':555},
+	{'id_parqueadero':'221', 'x':759, 'y':610},
+	{'id_parqueadero':'222', 'x':759, 'y':668},
+	{'id_parqueadero':'223', 'x':759, 'y':725},
+	{'id_parqueadero':'224', 'x':759, 'y':790},
+	{'id_parqueadero':'225', 'x':759, 'y':843},
+	{'id_parqueadero':'226', 'x':759, 'y':907},
+]
+
+export default zone3;
+
---
+++ constants/image_coordinates_zone_4.ts
@@ -0,0 +1,38 @@
+import {Zones} from "@/types";
+
+
+const zone4 : Zones = [
+	// A la izquierda de la imagen, desde arriba hasta abajo.
+	{'id_parqueadero':'229', 'x':342, 'y':100},
+	{'id_parqueadero':'230', 'x':342, 'y':148},
+	{'id_parqueadero':'231', 'x':342, 'y':195},
+	{'id_parqueadero':'232', 'x':342, 'y':242},
+	{'id_parqueadero':'233', 'x':342, 'y':290},
+	{'id_parqueadero':'234', 'x':342, 'y':337},
+	{'id_parqueadero':'235', 'x':342, 'y':384},
+	{'id_parqueadero':'236', 'x':342, 'y':432},
+	{'id_parqueadero':'237', 'x':342, 'y':478},
+	{'id_parqueadero':'238', 'x':342, 'y':526},
+	{'id_parqueadero':'239', 'x':342, 'y':713},
+	{'id_parqueadero':'240', 'x':342, 'y':760},
+	{'id_parqueadero':'241', 'x':342, 'y':804},
+	{'id_parqueadero':'242', 'x':342, 'y':854},
+	{'id_parqueadero':'243', 'x':342, 'y':904},
+	{'id_parqueadero':'244', 'x':342, 'y':950},
+	// A la derecha de la imagen, desde arriba hasta abajo.
+	{'id_parqueadero':'245', 'x':538, 'y':100},
+	{'id_parqueadero':'246', 'x':538, 'y':141},
+	{'id_parqueadero':'247', 'x':538, 'y':190},
+	{'id_parqueadero':'248', 'x':538, 'y':236},
+	{'id_parqueadero':'249', 'x':538, 'y':285},
+	{'id_parqueadero':'250', 'x':538, 'y':330},
+	{'id_parqueadero':'251', 'x':538, 'y':380},
+	{'id_parqueadero':'252', 'x':538, 'y':428},
+	{'id_parqueadero':'253', 'x':538, 'y':476},
+	{'id_parqueadero':'254', 'x':538, 'y':606},
+	{'id_parqueadero':'255', 'x':538, 'y':653},
+	{'id_parqueadero':'256', 'x':538, 'y':700},
+	{'id_parqueadero':'257', 'x':538, 'y':747},
+]
+
+export default zone4;
