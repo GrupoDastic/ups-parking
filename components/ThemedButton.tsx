770c591a2b7871bc152b7e5840c9ce6d46faab3e
Hugo Saldarriaga <hugosmm2000@gmail.com>
2025-02-18 08:20:36 -0500

    feat: Add new component for Button for each theme like dark or light

---
+++ components/ThemedButton.tsx
@@ -0,0 +1,24 @@
+import {Button, ButtonProps} from "react-native";
+import {useThemeColor} from "@/hooks/useThemeColor";
+
+export type ThemedButtonProps = ButtonProps & {
+    lightColor?: string;
+    darkColor?: string;
+    title: string;
+};
+
+export function ThemedButton({
+                                 title,
+                                 lightColor,
+                                 darkColor,
+                                 ...rest
+                             }: ThemedButtonProps) {
+    const color = useThemeColor({light: lightColor, dark: darkColor}, 'button');
+    
+    return (
+        <Button
+            color={color}
+            title={title}
+        />
+    );
+}
